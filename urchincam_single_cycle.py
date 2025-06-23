d213c720474b5c366742f50b57c5481fbe955d04
maya-ocean <mayaolin@hawaii.edu>
2025-06-23 12:46:46 -1000

    Added urchincam_single_cycle.py

---
+++ urchincam_single_cycle.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+import time
+from datetime import datetime, timedelta
+import subprocess
+import os
+import signal
+import sys
+
+# Create folder for recordings and log
+SESSION_FOLDER = "UrchinPOD"
+os.makedirs(SESSION_FOLDER, exist_ok=True)
+
+
+# Log file path
+LOG_FILE = os.path.join(SESSION_FOLDER, "urchin_log.txt")
+
+# Set duration of recording for a single on cycle
+DURATION_MINS = 27
+
+# Graceful exit flag
+should_exit = False
+
+def log(message):
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    formatted = f"[{timestamp}] {message}"
+    print (formatted)
+    with open (LOG_FILE, "a") as f:
+        f.write (formatted + "\n")
+
+def signal_handler (sig, frame):
+    global should_exit
+    print ("[!] Received interrupt signal. Preparing to exit gracefully...")
+    log ("[!] Received interrupt signal. Preparing to exit gracefully...")
+    should_exit = True
+
+signal.signal (signal.SIGINT, signal_handler)
+
+# Start camera recording for given duration in seconds
+def start_camera_recording(duration_secs):
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    h264_file = os.path.join(SESSION_FOLDER, f"video_{timestamp}.h264")
+    mp4_file = os.path.join(SESSION_FOLDER, f"video_{timestamp}.mp4")
+
+    print(f"[{timestamp}] Recording for {duration_secs} seconds → {mp4_file}")
+    log (f"Recording for {duration_secs} seconds → {mp4_file}")
+   
+    command = [
+        "libcamera-vid",
+        "-t", str(duration_secs * 1000),
+        "-o", h264_file,
+        "--inline",
+        "--nopreview"
+    ]
+
+    try:
+        subprocess.run(command, check=True)
+        
+        # Convert to mp4.
+        convert_command = [
+            "ffmpeg", "-y",
+            "-framerate", "30",
+            "-i", h264_file,
+            "-c", "copy",
+            mp4_file
+        ]
+        subprocess.run (convert_command, check = True)
+        os.remove (h264_file)
+        
+    except subprocess.CalledProcessError as e:
+        print (f"[{timestamp}] ERROR: Failed to record video.")
+        print (f"[{timestamp}] Command:", e.cmd)
+        print (f"[{timestamp}] Exit code:", e.returncode)
+        log ("ERROR: Failed to record video.")
+        log (f"Command: {e.cmd}")
+        log (f"Exit code: {e.returncode}")
+
+# Main loop
+def half_hour_cycle():  
+    duration_secs = DURATION_MINS * 60   
+    start_camera_recording(duration_secs)
+    print (f"[{datetime.now().strftime('%H:%M:%S')}] Video recorded.")
+    log (f"[{datetime.now().strftime('%H:%M:%S')}] Video recorded.")
+    time.sleep(10)
+
+# Start the scheduler
+half_hour_cycle ()
