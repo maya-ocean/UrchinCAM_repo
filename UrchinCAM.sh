ff46d87931a0c6a1ffd31cf9cf5185e3dee987c8
Maya <mayaolin@hawaii.edu>
2025-07-28 11:55:23 -1000

    Update UrchinCAM.sh
    assume auto-mount of external drive

--- UrchinCAM.sh
+++ UrchinCAM.sh
@@ -6,20 +6,6 @@ MOUNT_NAME="URCHIN2"
 # Log output
 exec > /home/pi/Urchin_log.txt 2>&1
 
-# Check if drive exists
-if [ ! -e /dev/sda1 ]; then
-    echo "/dev/sda1 not found. Exiting."
-    exit 1
-fi
-
-# Check if drive is mounted. 
-if ! mount | grep -q "/media/pi/$MOUNT_NAME"; then
-    # Mount drive if not mounted.
-    sudo mkdir -p /media/pi/$MOUNT_NAME
-    # The below line will only work if the drive is read by the pi as sda1. Make sure to only have one drive in the pi at a time.
-    sudo mount /dev/sda1 /media/pi/$MOUNT_NAME
-fi
-
 # Runs video recording in a loop 5 times.
 for (( i=0; i<5; i++ )); do
 echo "$i"
