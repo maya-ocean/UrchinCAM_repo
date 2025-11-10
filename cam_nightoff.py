9a04a8503c969f000908a4a3ebb0f9a92ab4802d
Maya <mayaolin@hawaii.edu>
2025-11-10 12:54:14 -1000

    Create cam_nightoff.py
    nested if/else into for loop to only run camera during daylight hours

---
+++ cam_nightoff.py
@@ -0,0 +1,27 @@
+import time
+from datetime import datetime
+from picamera2 import Picamera2
+
+TOTAL_HOURS = 72
+SEGMENT_MINS = 10
+thismany = int((TOTAL_HOURS * 60) / SEGMENT_MINS)
+
+cam = Picamera2()
+
+cam.video_configuration.controls.FrameRate = 30.0
+
+for i in range(thismany):
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    hour = datetime.now().hour
+
+    try:
+        if 6 <= hour <= 19:
+            cam.start_and_record_video(f"{timestamp}.mp4", duration=SEGMENT_MINS*60)
+        else:
+            time.sleep (duration = SEGMENT_MINS*60)
+
+    except KeyboardInterrupt:
+        break
+    
+    finally:
+        cam.stop_recording()
