a334e5afe748e4c6f9df618ce5914fcf12be3451
Maya <mayaolin@hawaii.edu>
2025-11-10 12:56:08 -1000

    Refactor daylight hour checks with constants
    Updated daylight hour checks to use constants for dawn and dusk.

--- cam_nightoff.py
+++ cam_nightoff.py
@@ -4,6 +4,8 @@ from picamera2 import Picamera2
 
 TOTAL_HOURS = 72
 SEGMENT_MINS = 10
+DAWN = 6
+DUSK = 19
 thismany = int((TOTAL_HOURS * 60) / SEGMENT_MINS)
 
 cam = Picamera2()
@@ -15,13 +17,16 @@ for i in range(thismany):
     hour = datetime.now().hour
 
     try:
-        if 6 <= hour <= 19:
+        # Record video segments during daylight hours
+        if DAWN <= hour <= DUSK:
             cam.start_and_record_video(f"{timestamp}.mp4", duration=SEGMENT_MINS*60)
+        # Sleep for duration and then try again during nighttime
         else:
             time.sleep (duration = SEGMENT_MINS*60)
 
     except KeyboardInterrupt:
         break
-    
+
+    # Stop recording after TOTAL_HOURS
     finally:
         cam.stop_recording()
