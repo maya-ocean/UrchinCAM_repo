f7d1509d964d0d7f36abaab94348fce508fc3d5d
Maya <mayaolin@hawaii.edu>
2025-11-10 12:58:54 -1000

    Refactor video recording loop with error handling

--- cam_nightoff.py
+++ cam_nightoff.py
@@ -12,21 +12,22 @@ cam = Picamera2()
 
 cam.video_configuration.controls.FrameRate = 30.0
 
-for i in range(thismany):
-    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    hour = datetime.now().hour
+try:
+    for i in range(thismany):
+        now = datetime.now()
+        timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
+        hour = now.hour
 
-    try:
         # Record video segments during daylight hours
         if DAWN <= hour <= DUSK:
             cam.start_and_record_video(f"{timestamp}.mp4", duration=SEGMENT_MINS*60)
         # Sleep for duration and then try again during nighttime
         else:
-            time.sleep (duration = SEGMENT_MINS*60)
+            time.sleep (SEGMENT_MINS*60)
 
-    except KeyboardInterrupt:
-        break
+except KeyboardInterrupt:
+    break
 
-    # Stop recording after TOTAL_HOURS
-    finally:
-        cam.stop_recording()
+# Stop recording after TOTAL_HOURS
+finally:
+    cam.stop_recording()
